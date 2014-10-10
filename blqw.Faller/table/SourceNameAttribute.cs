a1fa38afc25133a4e9c8fc49a5d721aba54f1aa7
jy02305022 <fttl_398@126.com>
2014-10-10 13:54:54 +0800

    增加一个特性

---
+++ blqw.Faller/table/SourceNameAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace blqw
+{
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public class SourceNameAttribute : Attribute
+    {
+        public SourceNameAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public static string GetName(MemberInfo member)
+        {
+            if (member == null)
+            {
+                throw new ArgumentNullException("member");
+            }
+            var attr = Attribute.GetCustomAttribute(member, typeof(Attribute));
+            return (attr == null) ? member.Name : ((SourceNameAttribute)attr).Name;
+        }
+    }
+}
